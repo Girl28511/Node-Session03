d99b21557e88d5500b5c90a04c04cbe60bcc4414
Girl28511 <Annabel-m@hotmail.co.uk>
2022-09-19 21:46:30 +0100

    task 2 express routing - currently not working need checking

--- src/index.js
+++ src/index.js
@@ -42,6 +42,42 @@ app.get("/", (req,res) => {
   res.send("Hello");
 });   
 
+// adding specific files to the send request rather than a message "hello" as above. Files must be absolute or specify root. Combine absolute paths. The .. goes back into the main folder or root then finds the file 
+app.get("/htmlfiledetails", (req,res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});   
+
+app.post("/test", (req,res) => {
+  res.send("THIS IS A POST REQUEST");
+});   
+
+// GET POST 
+
+app.all("/all-requests", (req,res) => {
+  res.send("This handles all methods");
+});   
+
+// Calling next function, it passes the control from one hanlder to another. It has to be the same handler e.g POST LEADS TO NEXT POST. They also have to have the same "details in the brackets"
+
+app.get("/api/:id", (req,res,next) => {
+  const id = req.params.id;
+
+  if (id == 2) {
+    res.send("This is ID user Andrew"); // if ID is 2 ends here 
+  } else {
+    next(); // if not 2, this is calling the next matched route which is a GET on line 76 
+}});   
+
+app.post("(/api/:id", (req,res,next) => {
+  res.send(" No user for this id:" + req.params.id); 
+});
+
+
+app.get("/api/:id", (req,res,next) => {
+  res.send("Hello"); // send Hello to the client and then the response 
+});
+
+
 // listen takes two parameters, and the second is callback function for when the server is running 
 app.listen(port, () => {
   console.log("Server is up and running") 
